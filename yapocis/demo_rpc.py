Fixed reach for gradient

yapocis/demo_rpc.py
@@ -44,7 +44,7 @@ def test():
     dog = blur-blurmore
     showArray("DOG", dog)
     
-    g,a = gradient(i,5)
+    g,a = gradient(i)
     showArray("Gradient",g)
     showArray("Angle", a)
     sat = np.ones_like(i)

yapocis/gradient.py
@@ -12,8 +12,8 @@ from rpc import kernels, interfaces
 program = kernels.loadProgram(interfaces.gradient, engine=kernels.GPU_ENGINE)
 gradientcl = program.gradient
 gradient_res = program.gradient_res
-def gradient(image,reach=1):
-    grad,theta = gradientcl(image, reach)
+def gradient(image):
+    grad,theta = gradientcl(image)
     # TODO: gradientCL should not be returning nans, and is
     theta[np.where(np.isnan(theta))] = 0.0
     return grad, theta